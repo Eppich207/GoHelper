b706bc9fb9ffe98f255272590d132b6a66ad5b5f
Eppich207 <mweppich@gmail.com>
2023-09-12 20:39:13 +0100

    +code

---
+++ part1/popup/popup.js
@@ -0,0 +1,22 @@
+const StartCountdown = document.getElementsById('sendMessageId');
+if (StartCountdown) {
+    StartCountdown.onclick = function () {
+        chrome.tabs.sendMEssage(
+            tabs[0].id,
+            {
+                url: chrome.runtime.getURL("images/stars.jpeg"),
+                imageDivId: `${guidGenerator()}`,
+                tabId: tabs[0].id
+            },
+            function(response) {
+                window.close();
+            }
+        );
+        function guidGenerator() {
+            const S4 = function () {
+                return (((1 + Math.random()) * 0x10000)| 0).toString(16).substring(1);
+            };
+            return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+        }
+    };
+};
