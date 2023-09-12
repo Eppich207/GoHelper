40cea7aeaf43288d4883e8bcd7abc207a836fca9
Eppich207 <mweppich@gmail.com>
2023-09-12 20:42:25 +0100

    Update popup.js

--- part1/popup/popup.js
+++ part1/popup/popup.js
@@ -1,22 +1,25 @@
-const StartCountdown = document.getElementsById('sendMessageId');
+
+const StartCountdown = document.getElementById("StartCountdown");
 if (StartCountdown) {
-    StartCountdown.onclick = function () {
-        chrome.tabs.sendMEssage(
-            tabs[0].id,
-            {
-                url: chrome.runtime.getURL("images/stars.jpeg"),
-                imageDivId: `${guidGenerator()}`,
-                tabId: tabs[0].id
-            },
-            function(response) {
-                window.close();
+    StartCountdown.onclick = function() {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            chrome.tabs.sendMessage(
+                tabs[0].id,
+                {
+                    url: chrome.runtime.getURL("images/stars.jpeg"),
+                    imageDivId: `${guidGenerator()}`,
+                    tabId: tabs[0].id
+                },
+                function(response) {
+                    window.close();
+                }
+            );
+            function guidGenerator() {
+                const S4 = function () {
+                    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+                };
+                return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
             }
-        );
-        function guidGenerator() {
-            const S4 = function () {
-                return (((1 + Math.random()) * 0x10000)| 0).toString(16).substring(1);
-            };
-            return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-        }
+        });
     };
-};
+}
