b5aab17ce71f22fa8186e65e0e742c5363226867
Eppich207 <mweppich@gmail.com>
2023-09-27 19:38:00 +0100

    Update

--- part1/popup/popup.js
+++ part1/popup/popup.js
@@ -2,8 +2,22 @@
 const StartCountdown = document.getElementById("StartCountdown");
 if (StartCountdown) {
     StartCountdown.onclick = function() {
-        var text = "This is a test";
+        var text = "This is a test1";
         navigator.clipboard.writeText(text).then(function() {
+           console.log('Async: Copying to clipboard was successful');
+        }, function(err) {
+            console.error(err);
+        
+    });
+
+}};
+
+
+const StartCountdown1 = document.getElementById("StartCountdown1");
+if (StartCountdown1) {
+    StartCountdown1.onclick = function() {
+        var text1 = "This is a test2";
+        navigator.clipboard.writeText(text1).then(function() {
            console.log('Async: Copying to clipboard was successful');
         }, function(err) {
             console.error(err);
