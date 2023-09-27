e83e5f97b4eb6392e511ca75754c83f287402315
Eppich207 <mweppich@gmail.com>
2023-09-27 21:29:47 +0100

    update v1.2

--- part1/popup/popup.js
+++ part1/popup/popup.js
@@ -1,27 +1,27 @@
 
+
 const StartCountdown = document.getElementById("StartCountdown");
 if (StartCountdown) {
     StartCountdown.onclick = function() {
-        var text = "This is a test1";
+        const textEditor = document.getElementById("textEditor");
+        const text = textEditor.value; // Get the current value of textEditor
+        navigator.clipboard.writeText(text).then(function() {
+            console.log('Async: Copying to clipboard was successful!');
+        }, function(err) {
+            console.error('Async: Could not copy text: ', err);
+        });
+    };
+}
+
+// Similar code for StartCountdown1, if needed
+const StartCountdown1 = document.getElementById("StartCountdown1");
+if (StartCountdown1) {
+    StartCountdown1.onclick = function() {
+        var text = "This is a test";
         navigator.clipboard.writeText(text).then(function() {
            console.log('Async: Copying to clipboard was successful');
         }, function(err) {
             console.error(err);
-        
-    });
-
-}};
-
-
-const StartCountdown1 = document.getElementById("StartCountdown1");
-if (StartCountdown1) {
-    StartCountdown1.onclick = function() {
-        var text1 = "This is a test2";
-        navigator.clipboard.writeText(text1).then(function() {
-           console.log('Async: Copying to clipboard was successful');
-        }, function(err) {
-            console.error(err);
-        
-    });
-
-}};
+        });
+    };
+}
