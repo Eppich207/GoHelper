7cd6eb47c93018302b0f45d3d4781faaaf24a2a3
Eppich207 <mweppich@gmail.com>
2023-09-27 19:27:43 +0100

    push

--- part1/popup/popup.js
+++ part1/popup/popup.js
@@ -2,24 +2,12 @@
 const StartCountdown = document.getElementById("StartCountdown");
 if (StartCountdown) {
     StartCountdown.onclick = function() {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(
-                tabs[0].id,
-                {
-                    url: chrome.runtime.getURL("images/stars.jpeg"),
-                    imageDivId: `${guidGenerator()}`,
-                    tabId: tabs[0].id
-                },
-                function(response) {
-                    window.close();
-                }
-            );
-            function guidGenerator() {
-                const S4 = function () {
-                    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-                };
-                return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-            }
-        });
-    };
-}
+        var text = "This is a test";
+        navigator.clipboard.writeText(text).then(function() {
+           console.log('Async: Copying to clipboard was successful');
+        }, function(err) {
+            console.error(err);
+        
+    });
+
+}};
