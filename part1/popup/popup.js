924018e8af552b6fd70cad30b6f0c73782ada7fa
Eppich207 <mweppich@gmail.com>
2023-09-29 18:06:24 +0100

    update v0.0.6
    Added css sheet

--- part1/popup/popup.js
+++ part1/popup/popup.js
@@ -1,13 +1,17 @@
 
 
+// Grabbing the textbox area and copying it
 const MSD = document.getElementById("MSD");
 if (MSD) {
+    // On click event for MSD
     MSD.onclick = function() {
         const textEditor = document.getElementById("textEditor");
-        const text = textEditor.value; // Get the current value of textEditor
+        const text = textEditor.value; 
+        // Console.log
         navigator.clipboard.writeText(text).then(function() {
-            console.log('Async: Copying to clipboard was successful!');
+            console.log('Async:Copied text');
         }, function(err) {
+            // Error flow
             console.error('Async: Could not copy text: ', err);
         });
     };
@@ -16,11 +20,14 @@ if (MSD) {
 
 const StartCountdown1 = document.getElementById("StartCountdown1");
 if (StartCountdown1) {
+    // On click event for the standard copy
     StartCountdown1.onclick = function() {
-        var text = "This is a test";
+        var text = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; // Best regards, Rick
+        // Console.log
         navigator.clipboard.writeText(text).then(function() {
            console.log('Async: Copying to clipboard was successful');
         }, function(err) {
+            // Error flow
             console.error(err);
         });
     };
@@ -28,7 +35,9 @@ if (StartCountdown1) {
 
 // Grabbing the current version from the manifest
 document.addEventListener('DOMContentLoaded', function () {
+    // Getting manifest
     let manifest = chrome.runtime.getManifest();
     let version = manifest.version;
+    //making sure that you can have text infront of it
     document.getElementById("version").textContent += version;
 });
