06ffe938c907a6caa20d3d280de75371e0db5791
Eppich207 <mweppich@gmail.com>
2024-06-19 22:15:44 +0100

    update 0.4.0
    Update 0.4.0

--- part1/popup/popup.js
+++ part1/popup/popup.js
@@ -1,11 +1,11 @@
 console.log("popup.js loaded");
 
 function SimpleCopy() {
-    console.log('Async:1');
+    
     const textEditor = document.getElementById("textEditor");
     const text = textEditor.value;
     navigator.clipboard.writeText(text).then(function() {
-        console.log('Async:Copied text');
+        console.log('Async:0');
     }, function(err) {
         console.error('Async: Could not copy text: ', err);
     });
@@ -21,13 +21,13 @@ function assignCopyFunction(buttonIds) {
 }
 
 function initializePopup() {
-    console.log('Initializing popup');
+
     const textEditor = document.getElementById("textEditor");
 
     const MDS = document.getElementById("MDS");
     if (MDS) {
         MDS.addEventListener('click', function() {
-            console.log('MDS clicked');
+
             let MSDstr = "Hi , ik ben Maarten van EY technology, ik zie een open ticket over . Hoe is de stand van zaken?";
             textEditor.value = MSDstr;
         });
@@ -37,7 +37,7 @@ function initializePopup() {
     const COP = document.getElementById("COP");
     if (COP) {
         COP.addEventListener('click', function() {
-            console.log('COP clicked');
+
             textEditor.value = "We tried to reach out, without any luck. We will be trying again next business day.";
         });
         COP.addEventListener('dblclick', SimpleCopy);
@@ -46,7 +46,7 @@ function initializePopup() {
     const CCL = document.getElementById("CCL");
     if (CCL) {
         CCL.addEventListener('click', function() {
-            console.log('CCL clicked');
+
             textEditor.value = "Mocht je nog wat tijd over hebben, kan je dan feedback geven in de mail de nu je kant op komt? En een hele fijne dag nog!";
         });
         CCL.addEventListener('dblclick', SimpleCopy);
@@ -55,7 +55,7 @@ function initializePopup() {
     const AWY = document.getElementById("AWY");
     if (AWY) {
         AWY.addEventListener('click', function() {
-            console.log('AWY clicked');
+
             textEditor.value = "Ik zie ook dat je momenteel niet beschikbaar bent. Wanneer heb je tijd om hier naar te kijken? Als ik geen antwoord ontvang kom ik later vandaag bij je terug";
         });
         AWY.addEventListener('dblclick', SimpleCopy);
@@ -64,7 +64,7 @@ function initializePopup() {
     const FUP = document.getElementById("FUP");
     if (FUP) {
         FUP.addEventListener('click', function() {
-            console.log('FUP clicked');
+
             const dateObj = new Date();
             textEditor.value = "FUP 1 " + dateObj.toDateString();
         });
@@ -106,7 +106,7 @@ function initializePopup() {
 }
 
 function initializeSettings() {
-    console.log('Initializing settings');
+    
     const saveButton = document.getElementById("saveFnText");
     if (saveButton) {
         saveButton.addEventListener('click', saveTextForFN);
@@ -122,44 +122,44 @@ function saveTextForFN() {
     const fnNumber = document.getElementById("fnSelector").value;
     const textEditor = document.getElementById("textEditor1");
     const text = textEditor.value;
-    console.log('Saving text for FN' + fnNumber);
+    
     let data = {};
     data[`FN${fnNumber}Text`] = text;
     chrome.storage.sync.set(data, function() {
-        console.log(`Text for FN${fnNumber} saved.`);
+        
     });
 }
 
 function loadTextForFN(fnNumber) {
-    console.log('Loading text for FN' + fnNumber);
+    
     chrome.storage.sync.get(`FN${fnNumber}Text`, function(result) {
         if (result[`FN${fnNumber}Text`]) {
             const textEditor = document.getElementById("textEditor");
             textEditor.value = result[`FN${fnNumber}Text`];
-            console.log(`Text for FN${fnNumber} loaded: ${result[`FN${fnNumber}Text`]}`);
+            
         } else {
-            console.log('No text found for FN' + fnNumber);
+            
         }
     });
 }
 
 function loadFnTextforSettings() {
-    console.log('Called loadFnTextforSettings');
+    
     const fnNumber = document.getElementById("fnSelector").value;
     const textEditor = document.getElementById("textEditor1");
-    console.log('Loading constants for FN' + fnNumber);
+    
     chrome.storage.sync.get(`FN${fnNumber}Text`, function(result) {
         if (result[`FN${fnNumber}Text`]) {
             textEditor.value = result[`FN${fnNumber}Text`];
-            console.log(`Text for FN${fnNumber} loaded in settings: ${result[`FN${fnNumber}Text`]}`);
+            
         } else {
-            console.log('No text found for FN' + fnNumber);
+            
         }
     });
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
+    
     if (document.getElementById('textEditor1')) {
         initializeSettings();
     } else {
@@ -167,7 +167,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
     const versionElement = document.getElementById("versionmanifest");
     if (versionElement) {
-        console.log('Loading version from manifest');
+        
         let manifest = chrome.runtime.getManifest();
         let version = manifest.version;
         versionElement.textContent += version;
